7cf83fe430c85d65064f20bd42c48d49d042944c
markustaskinen <markustaskinen@gmail.com>
2017-05-09 19:10:49 +0300

    another length fix

--- game_animation.js
+++ game_animation.js
@@ -98,7 +98,7 @@ function updateDifficulty() {
       difficultyText.setText("Vaikea");
       music = game.add.audio('hard');
       arrowSpeed = 7;
-      gameLength = 310;
+      gameLength = 330;
       bpm = 270;
       break;
   }
